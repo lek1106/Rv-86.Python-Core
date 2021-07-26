1175ed90f2fd3f71cb79e8dfccf9d68863d84856
unknown <C:\Users\erj\Desktop\4-  2 \     \password The Bat! 9.2.3\The Bat! 9.2.3 RePack (& Portable) by TryRooM\TheBatPortable\Data\Mail>
2021-07-26 13:07:58 +0300

    Homework_1

---
+++ Okhmak/HW2/hw1_py2.py
@@ -0,0 +1,20 @@
+#Task 1
+v1 = float(raw_input("Variable A? "))
+v2 = float(raw_input("Variable B? "))
+print "A+B", v1+v2
+print "A-B", v1-v2
+print "A*B", v1*v2
+print "A/B", v1/v2
+#Task 2
+name=raw_input("What is your name? ")
+age=raw_input("How old are you? ")
+city=raw_input("Where do you live? ")
+print "shalom", name,"!"
+print "Your age is", age,"!" 
+print "Wow, you live in a beautiful", city,"!"
+#Task 3
+print "   (----------------)"
+print "   (   **       **  )"
+print "   (                )"
+print "   (   ----_!_----  )"
+print "   (............... )"
