e1ebee8c22e27658c6fc585418fbeafd4f5b03bf
Oleh <ol.khmelovskyi@gmail.com>
2021-07-26 12:52:59 +0300

    first commit

---
+++ khmelovskyi/homework1_1.py
@@ -0,0 +1,16 @@
+a = int(input("Введи перше число: "))
+b = int(input("Введи друге число: "))
+
+#addition
+addition = a+b
+#subtraction
+subtraction = a-b
+#multiplication
+multiplication = a*b
+#division
+division = a/b
+
+print("addition. result: ", addition)
+print("subtraction. result: ", subtraction)
+print("multiplication. result: ", multiplication)
+print("division. result: ", division)
