f846ba94ee730b87c6dd432bc2b3b016bc871bde
Nazar <Nazar.Fomenko1@gmail.com>
2021-07-26 09:14:44 +0300

    The first HW

---
+++ Fomenko/HW2/Task1.py
@@ -0,0 +1,9 @@
+a = int(input('Specify the first number:'))
+b = int(input('Specify another number:'))
+
+c = a + b
+d = a - b
+e = a * b
+f = a / b
+
+print('addition -', c, ';', 'subtraction -', d, ';', 'multiplication -', e, ';', 'division -', f)
---
+++ Fomenko/HW2/Task2.py
@@ -0,0 +1,6 @@
+name = input('What is your name?:')
+print('Hello,', name)
+age = input('How old are you?:')
+print('Your age is', age)
+city = input('Where do you live?:')
+print('You live in', city)
