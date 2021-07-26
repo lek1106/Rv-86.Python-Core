a8e8011ff518bcecdbe878d6a811aac56c4969a5
Oleh <ol.khmelovskyi@gmail.com>
2021-07-26 12:54:29 +0300

    second commit

---
+++ khmelovskyi/homework1_2.py
@@ -0,0 +1,6 @@
+name = input("What is your name?: ")
+age = input("How old you are: ")
+place_of_living = input("Where do you live?: ")
+
+
+print("Hello, " + name + ", Your age is " + age + ", You live in " + place_of_living + ".")
